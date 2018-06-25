b46aa59046804017455f693f54b0b750a5242602
fanrenFRank <1778907544@qq.com>
2018-06-25 13:37:10 +0800

    no message

---
+++ WVersion_Fir.framework/Headers/WVersionDeal.h
@@ -0,0 +1,15 @@
+//
+//  WVersionDeal.h
+//  ZAP
+//
+//  Created by LWW on 2017/12/4.
+//  Copyright © 2017年 UN-ING. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WVersionDeal : NSObject
+
++ (void)CkeckAppStoreVersion:(BOOL)aBort withAppId:(NSString *)appId;
+
+@end
---
+++ WVersion_Fir.framework/Headers/WVersion_Fir.h
@@ -0,0 +1,20 @@
+//
+//  WVersion_Fir.h
+//  WVersion_Fir
+//
+//  Created by LWW on 2018/6/22.
+//  Copyright © 2018年 LWW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WVersion_Fir.
+FOUNDATION_EXPORT double WVersion_FirVersionNumber;
+
+//! Project version string for WVersion_Fir.
+FOUNDATION_EXPORT const unsigned char WVersion_FirVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WVersion_Fir/PublicHeader.h>
+
+#import <WVersion_Fir/WVersionDeal.h>
+
